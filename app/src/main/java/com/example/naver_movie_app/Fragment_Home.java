e3c68bedd0f5c82f8f5582246161694d887dc8fb
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-05-23 19:50:17 +0900

    feat: 아이템 클릭 시 웹뷰 액티비티로 이동하는 코드 추가

--- app/src/main/java/com/example/naver_movie_app/Fragment_Home.java
+++ app/src/main/java/com/example/naver_movie_app/Fragment_Home.java
@@ -1,5 +1,6 @@
 package com.example.naver_movie_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +15,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class Fragment_Home extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         RecyclerView homeRecyclerView = view.findViewById(R.id.homeRecyclerView);
-        RecyclerView.Adapter<MyViewHolder> homeAdapter;
+        HomeAdapter homeAdapter;
         RecyclerView.LayoutManager homeRecyclerViewLayoutManager = new LinearLayoutManager(getContext());
 
         homeRecyclerView.setLayoutManager(homeRecyclerViewLayoutManager);
@@ -28,7 +30,13 @@ public class Fragment_Home extends Fragment {
         ArrayList<RecyclerViewItem> homeDataSet = (ArrayList<RecyclerViewItem>) getArguments().getSerializable("homeDataSet");
 
         homeAdapter = new HomeAdapter(homeDataSet);
+        homeAdapter.setOnItemClickListener((view1, position) -> {
+            Intent intent = new Intent(view1.getContext(), MovieWebViewActivity.class);
+            intent.putExtra("url", homeDataSet.get(position).getLink());
+            startActivity(intent);
+        });
         homeRecyclerView.setAdapter(homeAdapter);
+
         return view;
     }
 }
