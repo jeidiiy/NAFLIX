52904f59ec3d9ad65f9edcbbdca1db2e3fb78174
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-05-23 17:38:54 +0900

    feat: Home Fragment 구현

--- app/src/main/java/com/example/naver_movie_app/Fragment_Home.java
+++ app/src/main/java/com/example/naver_movie_app/Fragment_Home.java
@@ -8,14 +8,27 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class Fragment_Home extends Fragment {
-    ViewGroup viewGroup;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
-        return viewGroup;
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        RecyclerView homeRecyclerView = view.findViewById(R.id.homeRecyclerView);
+        RecyclerView.Adapter<MyViewHolder> homeAdapter;
+        RecyclerView.LayoutManager homeRecyclerViewLayoutManager = new LinearLayoutManager(getContext());
+
+        homeRecyclerView.setLayoutManager(homeRecyclerViewLayoutManager);
+
+        assert getArguments() != null;
+        ArrayList<RecyclerViewItem> homeDataSet = (ArrayList<RecyclerViewItem>) getArguments().getSerializable("homeDataSet");
+
+        homeAdapter = new HomeAdapter(homeDataSet);
+        homeRecyclerView.setAdapter(homeAdapter);
+        return view;
     }
 }
