26aa425c658e892aa634fddcf789772de75959fc
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-05-24 23:35:30 +0900

    feat: SQLite 헬퍼 클래스 구현

---
+++ app/src/main/java/com/example/naver_movie_app/MovieDBHelper.java
@@ -0,0 +1,22 @@
+package com.example.naver_movie_app;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MovieDBHelper extends SQLiteOpenHelper {
+    public MovieDBHelper(Context context) {
+        super(context, "movie", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE movie (imageSrc VARCHAR(100) PRIMARY KEY, title VARCHAR(30), director VARCHAR(30), actors VARCHAR(80), rating INTEGER, link VARCHAR(200));");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS movie;");
+        onCreate(db);
+    }
+}
